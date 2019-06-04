5f768abce876eadafffef6d7ae39c7b1954af24b
Blake <2979337714@qq.com>
2019-06-04 18:17:29 +0800

    input 组件

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input',Input)
 
 
 new Vue({
@@ -18,90 +20,4 @@ new Vue({
   }
 })
 
-//单元测试  断言
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
 
-const expect = chai.expect
-
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  vm.$mount('#test')
-  let useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.eq('#icon-setting')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      loading: true
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  expect(useElement.getAttribute('xlink:href')).to.eq('#icon-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{ 
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{ 
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      iconPosition: 'right'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
-
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting'
-    }
-  })
-  vm.$mount()
-  let spy = chai.spy(function(){})
-
-  vm.$on('click',spy)
-
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
-}
---
+++ src/input.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <icon name="error" class="icon-error"></icon>
+      <span class="errorMessage">{{error}}</span>
+    </template>
+  </div>
+</template>
+<script>
+import Icon from "./icon";
+export default {
+  name: "GInput",
+  components: {
+    icon: Icon
+  },
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$disalbed-color: #bbb;
+$red: #f1453d;
+
+.wrapper {
+  display: inline-flex;
+  align-items: center;
+  :not(:last-child){
+    margin-right: .5em;
+  }
+  input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: 4px;
+    padding: 9px 8px;
+    font-size: inherit;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: $box-shadow-color;
+      outline: none;
+    }
+    &[disabled] {
+      border-color: $disalbed-color;
+      color: $disalbed-color;
+      cursor: not-allowed;
+    }
+    &[readonly] {
+      border-color: $disalbed-color;
+      color: $disalbed-color;
+    }
+  }
+  &.error {
+    input {
+      border-color: $red;
+    }
+  }
+  .icon-error{
+    fill: $red;
+  }
+  .errorMessage{
+    color: $red;
+  }
+}
+</style>
