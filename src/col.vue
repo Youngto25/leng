a021641dfc15c08e3aaa78b2991c2aa34f8ba762
Blake <2979337714@qq.com>
2019-06-05 18:35:23 +0800

    重构 row and col

--- src/col.vue
+++ src/col.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="col" 
-  :class="[span && `col-${span}`,offset && `offset-${offset}`]" 
-  :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}"
+  :class="colClass" 
+  :style="colStyle"
   >
-  <div style="border: 1px solid red;height: 100px">
+  
     <slot></slot>
-  </div>
    
   </div>
 </template>
@@ -22,6 +21,19 @@ export default {
   data(){
     return {
       gutter: 0
+    }
+  },
+  computed: {
+    colClass(){
+      let {span,offset} = this
+      return [span && `col-${span}`,offset && `offset-${offset}`]
+    },
+    colStyle(){
+      return{
+        paddingLeft: this.gutter/2+'px',
+        paddingRight: this.gutter/2+'px'
+      }
+      
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row" 
-  :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}"
+  :style="rowStyle"
   >
     <slot><slot>
   </div>
@@ -16,6 +16,14 @@ export default {
     this.$children.forEach((vm)=>{
       vm.gutter = this.gutter
     })
+  },
+  computed: {
+    rowStyle(){
+      let {gutter} = this
+      return {
+        marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'
+      }
+    }
   }
 }
 </script>
