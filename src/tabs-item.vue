ad5267f93ad3076620a417f48ea96178a20bc71d
Blake <2979337714@qq.com>
2019-06-11 16:56:08 +0800

    优化可交互区域

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -48,8 +48,14 @@ export default {
 .tabs-item{
   flex-shrink: 0;
   padding: 0 1em;
+  height: 100%;
+  display: flex;
+  align-items: center;
   &.active{
     background: red;
+  }
+  &:hover{
+    cursor: pointer;
   }
 }
 </style>
