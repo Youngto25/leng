45ea558396057a06feb2ac4284e220f0914beed3
Blake <2979337714@qq.com>
2019-06-12 13:22:47 +0800

    创建了 popover 组件

---
+++ src/popover.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="popover" @click="xxx">
+    <div v-if="visible" class="content-wrapper">
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    visible: false
+  },
+  methods: {
+    xxx(){
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.popover{
+  display: inline-block;
+  position: relative;
+  .content-wrapper{
+    position: absolute;
+    bottom: 100%;
+    left: 0;
+  }
+}
+</style>
