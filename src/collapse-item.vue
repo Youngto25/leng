74712c091f60a613e2aa73328c479c105bff5e09
Blake <2979337714@qq.com>
2019-06-15 10:37:46 +0800

    collapse基本样式和基本功能完成

---
+++ src/collapse-item.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="collapseItem">
+    <div class="title" @click="toggle">{{title}}</div>
+    <div class="content" v-if="visible">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods:{
+    close(){
+      this.visible = false
+    },
+    open(){
+      this.visible = true
+    },
+    toggle(){
+      if(this.visible === true){
+        this.close()
+      }else{
+        this.open()
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+ $grey: #ddd;
+$border-radius: 4px;
+.collapseItem {
+    > .title {
+      border: 1px solid $grey;
+      margin-top: -1px;
+      margin-left: -1px;
+      margin-right: -1px;
+      min-height: 32px;
+      display: flex;
+      align-items: center;
+      padding: 0 8px;
+    }
+    &:first-child {
+      > .title {
+        border-top-left-radius: $border-radius;
+        border-top-right-radius: $border-radius;
+      }
+    }
+    &:last-child {
+      > .title:last-child {
+        border-bottom-left-radius: $border-radius;
+        border-bottom-right-radius: $border-radius;
+      }
+    }
+    > .content {
+      padding: 8px;
+    }
+  }
+</style>
