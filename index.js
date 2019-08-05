956b1285382485025d0b89b29ec39fcdd7d96ea7
Blake <2979337714@qq.com>
2019-08-05 17:23:56 +0800

    优化代码结构

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 import Button from './src/button/button'
 import ButtonGroup from './src/button/button-group'
 import Icon from './src/icon/icon'
+import Input from './src/input/input'
+import Row from './src/grid/row'
+import Col from './src/grid/col'
+import Layout from './src/layout/layout'
+import Sider from './src/layout/sider'
+import Content from './src/layout/content'
+import Header from './src/layout/header'
+import Footer from './src/layout/footer'
+import Toast from './src/toast'
+import plugin from './src/plugin'
+import Tabs from './src/tab/tabs'
+import TabsHead from './src/tab/tabs-head'
+import TabsItem from './src/tab/tabs-item'
+import TabsBody from './src/tab/tabs-body'
+import TabsPane from './src/tab/tabs-pane'
+import Popover from './src/popover'
+import Collapse from './src/collapse'
+import CollapseItem from './src/collapse-item'
 
-export {Button,ButtonGroup,Icon}
+export {
+  Button,
+  ButtonGroup,
+  Icon,
+  Input,
+  Row,
+  Col,
+  Layout,
+  Sider,
+  Content,
+  Header,
+  Footer,
+  Toast,
+  plugin,
+  Tabs,
+  TabsBody,
+  TabsHead,
+  TabsItem,
+  TabsPane,
+  Popover,
+  Collapse,
+  CollapseItem
+}
